948603446c20f5187429c7e3d653166dd8350333
jakob153 <guenayjakob@gmail.com>
2020-06-17 03:40:50 +0200

    add missing custom ApolloProvider

---
+++ src/ApolloProviderWithToken.tsx
@@ -0,0 +1,35 @@
+import React, { FC, useContext } from 'react';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/link-context';
+
+import { UserContext } from './components/user/UserContext';
+
+const ApolloProviderWithToken: FC = ({ children }) => {
+  const { user } = useContext(UserContext);
+
+  const http = createHttpLink({
+    uri: process.env.REACT_APP_GRAPHQL_API,
+    credentials: 'include',
+  });
+
+  const authLink = setContext((_, { headers }) => ({
+    headers: {
+      ...headers,
+      ...(user.authToken && { authorization: `Bearer ${user.authToken}` }),
+    },
+  }));
+
+  const apolloClient = new ApolloClient({
+    link: authLink.concat(http),
+    cache: new InMemoryCache(),
+  });
+
+  return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
+};
+
+export default ApolloProviderWithToken;
