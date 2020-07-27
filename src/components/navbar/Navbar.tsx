d5935c6b86fa04c76a2d8c82082645ae195af8c2
jakob153 <guenayjakob@gmail.com>
2020-07-28 01:50:31 +0200

    handle logout out of graphql

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,14 +8,11 @@ import {
   Typography,
 } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
 import { makeStyles } from '@material-ui/core/styles';
 
 import AuthModal from '../authModal/AuthModal';
 
 import { UserContext } from '../user/UserContext';
-
-import { LOGOUT_MUTATION } from './LogOut.mutation';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -39,8 +36,6 @@ const NavBar: FC = () => {
   });
   const classes = useStyles();
 
-  const [logOutMutation] = useMutation(LOGOUT_MUTATION);
-
   const handleClick = (selectedTab: number) => () => {
     setShowModal((prevState) => ({ open: !prevState.open, selectedTab }));
   };
@@ -49,10 +44,17 @@ const NavBar: FC = () => {
     setShowModal((prevState) => ({ open: !prevState.open, selectedTab: null }));
   };
 
-  const handleLogout = () => {
+  const handleLogout = async () => {
     setUser({ username: '', email: '', loggedIn: false, authToken: '' });
 
-    logOutMutation();
+    try {
+      await fetch('/refreshToken', {
+        method: 'DELETE',
+        credentials: 'include',
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const { open, selectedTab } = showModal;
