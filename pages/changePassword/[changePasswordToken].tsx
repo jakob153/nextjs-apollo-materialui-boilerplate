9eb2f8d26cf4373b63872000f9645db327e96aca
jakob153 <guenayjakob@gmail.com>
2021-02-13 21:34:23 +0100

    add changePassword page

---
+++ pages/changePassword/[changePasswordToken].tsx
@@ -0,0 +1,153 @@
+import React, { ChangeEvent, FC, FormEvent, useContext, useState } from 'react';
+import {
+  Button,
+  Paper,
+  Snackbar,
+  TextField,
+  Typography,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
+import { gql, useMutation } from '@apollo/client';
+import { useRouter } from 'next/router';
+
+import { UserContext } from '../../components/context/UserContext';
+
+import { SnackbarState } from '../../types';
+
+const CHANGE_PASSWORD = gql`
+  mutation changePassword(
+    $newPassword: String!
+    $changePasswordToken: String!
+  ) {
+    changePassword(
+      newPassword: $newPassword
+      changePasswordToken: $changePasswordToken
+    )
+  }
+`;
+
+const useStyles = makeStyles((theme: Theme) => ({
+  paper: {
+    padding: theme.spacing(3),
+    maxWidth: 600,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  title: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+  },
+  marginBottom5: {
+    marginBottom: theme.spacing(5),
+  },
+}));
+
+const ChangePassword: FC = () => {
+  const { user } = useContext(UserContext);
+  const router = useRouter();
+
+  const [form, setForm] = useState({ password: '', password2: '' });
+  const [snackbar, setSnackbar] = useState<SnackbarState>({
+    message: '',
+    show: false,
+  });
+  const classes = useStyles();
+
+  const [changePassword] = useMutation<
+    boolean,
+    { newPassword: string; changePasswordToken: string }
+  >(CHANGE_PASSWORD, {
+    onCompleted: () => {
+      setSnackbar({
+        message: 'Password changed',
+        show: true,
+      });
+
+      router.push('/');
+    },
+    onError: () => {
+      setSnackbar({
+        message: 'Something went wrong!',
+        show: true,
+      });
+    },
+  });
+
+  const handleChange = (
+    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const { name, value } = event.target;
+
+    setForm((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    changePassword({
+      variables: {
+        newPassword: form.password2,
+        changePasswordToken: router.query.changePasswordToken as string,
+      },
+    });
+  };
+
+  if (user.loggedIn) {
+    router.push('/');
+  }
+
+  return (
+    <>
+      <Typography className={classes.title} variant="h5" align="center">
+        Change Your Password
+      </Typography>
+      <Paper className={classes.paper}>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            autoComplete="new-password"
+            name="password"
+            className={classes.marginBottom5}
+            label="Password"
+            type="password"
+            onChange={handleChange}
+            value={form.password}
+            variant="outlined"
+            fullWidth
+          />
+          <TextField
+            autoComplete="new-password"
+            name="password2"
+            className={classes.marginBottom5}
+            label="Confirm Password"
+            type="password"
+            onChange={handleChange}
+            value={form.password2}
+            variant="outlined"
+            fullWidth
+          />
+          <Button
+            type="submit"
+            disabled={
+              (form.password.length === 0 && form.password2.length === 0) ||
+              form.password !== form.password2
+            }
+            variant="contained"
+            fullWidth
+          >
+            Change password
+          </Button>
+        </form>
+      </Paper>
+      <Snackbar
+        open={snackbar.show}
+        onClose={() =>
+          setSnackbar((prevState) => ({ ...prevState, show: false }))
+        }
+        message={snackbar.message}
+      />
+    </>
+  );
+};
+
+export default ChangePassword;
---
+++ pages/requestChangePassword.tsx
@@ -0,0 +1,110 @@
+import React, { ChangeEvent, FC, FormEvent, useState } from 'react';
+import {
+  Button,
+  Paper,
+  Snackbar,
+  TextField,
+  Typography,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
+import { gql, useMutation } from '@apollo/client';
+
+import { SnackbarState } from '../types';
+
+const SEND_CHANGE_PASSWORD_MAIL = gql`
+  mutation sendChangePasswordMail($email: String!) {
+    sendChangePasswordMail(email: $email)
+  }
+`;
+
+const useStyles = makeStyles((theme: Theme) => ({
+  paper: {
+    padding: theme.spacing(3),
+    maxWidth: 600,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  title: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+  },
+  marginBottom5: {
+    marginBottom: theme.spacing(5),
+  },
+}));
+
+const RequestChangePassword: FC = () => {
+  const [email, setEmail] = useState('');
+  const [snackbar, setSnackbar] = useState<SnackbarState>({
+    message: '',
+    show: false,
+  });
+  const classes = useStyles();
+
+  const [sendChangePasswordMail] = useMutation<boolean, { email: string }>(
+    SEND_CHANGE_PASSWORD_MAIL,
+    {
+      onCompleted: () => {
+        setSnackbar({
+          message:
+            'A Password Reset Link was sent was sent to your mail address.',
+          show: true,
+        });
+      },
+      onError: () => {
+        setSnackbar({
+          message: 'Something went wrong!',
+          show: true,
+        });
+      },
+    }
+  );
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    sendChangePasswordMail({
+      variables: { email },
+    });
+  };
+
+  return (
+    <>
+      <Typography className={classes.title} variant="h5" align="center">
+        Reset Your Password
+      </Typography>
+      <Paper className={classes.paper}>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            name="email"
+            className={classes.marginBottom5}
+            label="Email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            variant="outlined"
+            fullWidth
+          />
+          <Button
+            type="submit"
+            disabled={email === ''}
+            variant="contained"
+            fullWidth
+          >
+            Send change password mail
+          </Button>
+        </form>
+      </Paper>
+      <Snackbar
+        open={snackbar.show}
+        onClose={() =>
+          setSnackbar((prevState) => ({ ...prevState, show: false }))
+        }
+        message={snackbar.message}
+      />
+    </>
+  );
+};
+
+export default RequestChangePassword;
